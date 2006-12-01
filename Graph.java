e811530a8e1dae4874bcfbc3406a868ceef80a1c
Igor Serebryany <igor47@moomers.org>
2006-12-01 04:38:33 +0000

    started java version

---
+++ Graph.java
@@ -0,0 +1,10 @@
+
+public interface GraphRepresentation {
+    public void addEdge(v1,v2,dist);
+    public int dist(v1,v2);
+    public boolean isNode(v);
+    public boolean hasEdge(v1,v2);
+}
+
+//public class AdjacencyList implements GraphRepresentation {
+
---
+++ Trains.java
@@ -0,0 +1,15 @@
+public class Trains {
+	public static void main(String args[])
+	{
+		System.out.println("You requested " + args[0]);
+	}
+	
+	private String readGraph(String filename)
+	{
+		return "";
+	}
+
+	public int routeDistance throws NoSuchRoute{
+		return 0;
+	}
+}
